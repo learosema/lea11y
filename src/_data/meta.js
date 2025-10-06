22afe3f83b348df4aa9bf842092d4ee158d4386d
Lea Rosema <lea@lea.lgbt>
2025-10-06 10:31:12 +0200

    fix: remove double slash issue

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,5 @@
 export default {
   siteName: 'lea11y',
   author: 'Lea Rosema',
-  root: process.env.ENV === 'production' ? 'https://learosema.github.io/lea11y/' : ''
+  root: process.env.ENV === 'production' ? 'https://learosema.github.io/lea11y' : ''
 }
